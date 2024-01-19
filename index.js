7947346e93a49587f2c4c002dc4ea98d8013d602
dulgx <dulguunnu@gmail.com>
2024-01-19 11:08:55 +0800

    email input showing in thank you

--- index.js
+++ index.js
@@ -2,19 +2,23 @@ const submit = document.getElementById("submit");
 const undsen = document.getElementById("main");
 const thanks = document.getElementById("thankyou");
 const emails = document.getElementById("email");
-const userMail = document.getElementById("user-mail").textContent;
+const uMail = document.getElementById("user-mail");
 
+emails.addEventListener("input", function () {
+    event.preventDefault();
+    var userInMail = emails.value;
+    uMail.textContent = userInMail;
+});
 
 submit.addEventListener("click", function () {
+    event.preventDefault();
     undsen.style.display = "none";
     thanks.style.display = "block";
 });
 thanks.addEventListener("click", function () {
+    event.preventDefault();
+    emails.value = "";
     thanks.style.display = "none";
     undsen.style.display = "";
 });
 
-emails.addEventListener("input", function () {
-    var userInMail = emails.value;
-    console.log(userInMail);
-});
