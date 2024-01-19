06ed98620ded962ac658df1ecb22675af8920202
dulgx <dulguunnu@gmail.com>
2024-01-19 10:54:44 +0800

    email input console

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const submit = document.getElementById("submit");
 const undsen = document.getElementById("main");
 const thanks = document.getElementById("thankyou");
+const emails = document.getElementById("email");
+const userMail = document.getElementById("user-mail").textContent;
+
 
 submit.addEventListener("click", function () {
     undsen.style.display = "none";
@@ -9,4 +12,9 @@ submit.addEventListener("click", function () {
 thanks.addEventListener("click", function () {
     thanks.style.display = "none";
     undsen.style.display = "";
-})
+});
+
+emails.addEventListener("input", function () {
+    var userInMail = emails.value;
+    console.log(userInMail);
+});
